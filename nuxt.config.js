9c841225445c847ea0ffe81d9ec59990b688417b
Colton Eakins <coltoneakins@gmail.com>
2018-12-18 19:51:57 -0600

    Add responsive SVG logo.

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: "Colton Eakins // Modern Front End Developer // Portfolio and Blog",
+    title: "Colton Eakins // Modern Front-End Developer // Portfolio and Blog",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
