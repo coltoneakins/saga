9590b9236ccb849632e134785a3389ae0eeb94ae
Colton Eakins <coltoneakins@gmail.com>
2019-01-13 20:54:26 -0600

    Remove the explicit reCaptcha on the Netlify Form for the Contact page.

--- pages/contact.vue
+++ pages/contact.vue
@@ -51,8 +51,6 @@
           </div>
         </div>
 
-        <div data-netlify-recaptcha="true"/>
-
         <div class="field is-grouped">
           <div class="control">
             <button class="button is-link" type="submit">Submit</button>
